c9b8c4efbbc9ce45a391d179d421fc4c5c576655
laxith278 <114569489+laxith278@users.noreply.github.com>
2024-08-05 02:18:41 +0530

    Update StudentDatabaseApp.java

--- StudentDatabaseApp.java
+++ StudentDatabaseApp.java
@@ -1,7 +1,21 @@
 package studentdatabaseapp
+  import java.util.Scanner;
   public class StudentDatabaseApp{
     public static void main(String[] args){
-      Student stud = new Student();
-      Student.enroll();
+      System.out.print("enter no. of new students to enroll:");
+       Scanner sc = new Scanner(System.in);
+      int numofstudents = sc.nextInt();
+      Student[] students = new Student[numofstudents];
+
+      for(int n=0;n<numofstudents;n++){
+        students[n]=new Student();
+        students[n].enroll();
+         students[n].paytuition();
+      }
+      for(int n=0;n<numofstudents;n++){
+        System.out.println(students[n].toString()); 
+      }
+      
+
     }
   }
